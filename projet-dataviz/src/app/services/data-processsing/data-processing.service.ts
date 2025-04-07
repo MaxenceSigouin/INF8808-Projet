d9efecfa7fca1682b9f2f9e0d94699e0be18dfe2
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-04-07 12:36:09 -0400

    feat: add csvData and a getter

--- projet-dataviz/src/app/services/data-processsing/data-processing.service.ts
+++ projet-dataviz/src/app/services/data-processsing/data-processing.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import * as d3 from 'd3';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataProcessingService {
-  constructor(private http: HttpClient) {
-    // this.http
-    //   .get('../../assets/nhldraft.csv', { responseType: 'text' })
-    //   .subscribe((data) => console.log(data)); // Erreur 404 pcq il cherche dans localhost:4200/assets/nhldraft.csv
+  private csvData: any;
+  constructor() {
+    this.loadCSVData();
+  }
+  private loadCSVData() {
+    d3.csv('assets/nhldraft.csv').then((data) => {
+      this.csvData = data;
+    });
+  }
 
-    d3.csv('assets/nhldraft.csv').then((data) => console.log(data));
+  getData() {
+    return this.csvData;
   }
 }
