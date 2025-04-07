0a64a81aeeecfe5ee192337cfa29eeb2a8cc8165
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-04-07 12:09:45 -0400

    fix: add assets folder to angular.json to tell angular to include assets folder in th build

--- projet-dataviz/src/app/services/data-processsing/data-processing.service.ts
+++ projet-dataviz/src/app/services/data-processsing/data-processing.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import * as d3 from 'd3';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataProcessingService {
-
   constructor(private http: HttpClient) {
-    this.http.get('assets/nhldraft.csv', {responseType: 'text'}).subscribe(data => console.log(data)); // Erreur 404 pcq il cherche dans localhost:4200/assets/nhldraft.csv
-    d3.csv('assets/nhldraft.csv').then(data => console.log(data)); // Erreur 404 pcq il cherche dans localhost:4200/assets/nhldraft.csv
+    // this.http
+    //   .get('../../assets/nhldraft.csv', { responseType: 'text' })
+    //   .subscribe((data) => console.log(data)); // Erreur 404 pcq il cherche dans localhost:4200/assets/nhldraft.csv
+
+    d3.csv('assets/nhldraft.csv').then((data) => console.log(data));
   }
 }
