928a884a6ef83ea90a10333cb9cfbbb073eba149
WillyMart <william.martin@polymtl.ca>
2025-04-08 07:53:14 -0400

    bar-chart page created

--- projet-dataviz/src/app/app.routes.ts
+++ projet-dataviz/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BarChartComponent } from './pages/bar-chart/bar-chart.component';
 
-export const routes: Routes = [{ path: '', component: HomePageComponent }];
+export const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'bar-chart', component: BarChartComponent }
+];
+
