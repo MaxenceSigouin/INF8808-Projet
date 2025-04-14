98517bb129adb572d6e8d543ceae3d18ddfb22ab
Alexandre Veillette <alexandreveillette.av@gmail.com>
2025-04-14 13:57:19 -0400

    feat: moving bar chart component to component folder

--- projet-dataviz/src/app/app.routes.ts
+++ projet-dataviz/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { BarChartComponent } from './pages/bar-chart/bar-chart.component';
+import { BarChartComponent } from './components/bar-chart/bar-chart.component';
 
 export const routes: Routes = [
-    { path: '', component: HomePageComponent },
-    { path: 'bar-chart', component: BarChartComponent }
+  { path: '', component: HomePageComponent },
+  { path: 'bar-chart', component: BarChartComponent },
 ];
-
