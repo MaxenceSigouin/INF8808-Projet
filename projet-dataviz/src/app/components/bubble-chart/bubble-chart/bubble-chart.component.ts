12832f62b44018a88fed25c4d64a86eca8df9112
Modest040 <leduccedryc@gmail.com>
2025-04-22 17:13:28 -0400

    Co-pilot refractor

--- projet-dataviz/src/app/components/bubble-chart/bubble-chart/bubble-chart.component.ts
+++ projet-dataviz/src/app/components/bubble-chart/bubble-chart/bubble-chart.component.ts
@@ -14,17 +14,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './bubble-chart.component.css',
 })
 export class BubbleChartComponent {
-  // private rowSpacing: number = 70;
   isTotalShow = true;
   private yearGroups: string[] = [];
   private countByCountry: Record<string, Record<string, number>> = {};
   private totalByCountry: Record<string, number> = {};
-  private yScale: d3.ScalePoint<string> = d3.scalePoint();
-  private xScale: d3.ScalePoint<string> = d3.scalePoint();
-  private xScaleCollapse: d3.ScalePoint<string> = d3.scalePoint();
-  private sizeScale: d3.ScaleLinear<number, number> = d3.scaleLinear();
-  private allYears: Set<number> = new Set();
-  private maxDomainValue: number = 0;
+  private yScale = d3.scalePoint<string>();
+  private xScale = d3.scalePoint<string>();
+  private xScaleCollapse = d3.scalePoint<string>();
+  private sizeScale = d3.scaleLinear<number, number>();
+  private allYears = new Set<number>();
+  private maxDomainValue = 0;
   private margin = { top: 40, right: 300, bottom: 40, left: 300 };
 
   constructor(private dataSrv: DataProcessingService) {}
@@ -32,130 +31,62 @@ export class BubbleChartComponent {
   ngAfterViewInit() {
     this.preprocess().then((country) => {
       this.countByCountry = country;
-      this.buildGraph();
-      this.setYScale();
-      this.drawYScale();
-      this.setXScale();
-      this.setXScaleCollapse();
-      if (this.isTotalShow) {
-        this.drawXScaleCollapse();
-      } else {
-        this.drawXScale();
-      }
-      this.setMaxDomainValue();
-
-      this.drawData();
-      this.addTooltip();
-      this.removeScaleLine();
+      this.initializeChart();
     });
   }
 
-  async preprocess(): Promise<Record<string, Record<string, number>>> {
-    return this.dataSrv.getData().then((data) => {
-      if (data) {
-        this.getAllYear(data);
-        this.getYearGroup(6);
+  private async preprocess(): Promise<Record<string, Record<string, number>>> {
+    const data = await this.dataSrv.getData();
+    if (!data) return {};
 
-        const countByCountry = Object.entries(
-          data.reduce((acc: Record<string, Record<string, number>>, row) => {
-            let rowYearGroup = '';
-            for (const yearGroup of this.yearGroups) {
-              const [start, end] = yearGroup.split('-').map(Number);
-              const year = Number(row['year']);
-              if (year >= start && year <= end) {
-                rowYearGroup = yearGroup;
-                break;
-              }
-            }
+    this.extractYears(data);
+    this.generateYearGroups(6);
 
-            if (!acc[row['nationality']]) {
-              acc[row['nationality']] = {};
-            }
-            acc[row['nationality']][rowYearGroup] =
-              (acc[row['nationality']][rowYearGroup] || 0) + 1;
+    const countByCountry = this.aggregateDataByCountry(data);
+    this.totalByCountry = this.calculateTotalByCountry(countByCountry);
 
-            this.totalByCountry[row['nationality']] =
-              (this.totalByCountry[row['nationality']] || 0) + 1;
-            return acc;
-          }, {})
-        ).sort((a, b) => this.totalByCountry[b[0]] - this.totalByCountry[a[0]]);
-
-        const top7 = countByCountry.slice(0, 7);
-
-        const othersCount = countByCountry
-          .slice(7)
-          .reduce((acc: Record<string, number>, entry) => {
-            Object.entries(entry[1]).forEach(([key, value]) => {
-              acc[key] = acc[key] + value || value;
-            });
-            return acc;
-          }, {});
-
-        const totalSort = Object.entries(this.totalByCountry).sort(
-          (a, b) => b[1] - a[1]
-        );
-        const totalTop7 = totalSort.slice(0, 7);
-
-        const totalOthersCount = totalSort
-          .slice(7)
-          .reduce((acc: number, entry) => {
-            return acc + entry[1];
-          }, 0);
-
-        this.totalByCountry = Object.fromEntries(
-          totalOthersCount !== null
-            ? totalTop7.concat([['Other', totalOthersCount]])
-            : totalTop7
-        );
-
-        return Object.fromEntries(
-          othersCount !== null ? top7.concat([['Other', othersCount]]) : top7
-        );
-      } else return {};
-    });
+    return this.filterTopCountries(countByCountry);
   }
 
-  buildGraph() {
+  private initializeChart() {
+    this.buildGraph();
+    this.setYScale();
+    this.drawYScale();
+    this.setXScales();
+    this.drawXScale();
+    this.updateChart();
+  }
+
+  private buildGraph() {
     const containerNode = d3
       .select('#bubble-chart-container')
       .node() as HTMLElement;
+    if (!containerNode) return;
 
-    if (containerNode) {
-      const containerBound = containerNode.getBoundingClientRect();
-      d3.select('#bubble-chart-container')
-        .append('svg')
-        .attr('id', 'bubble-chart-svg')
-        .attr('width', containerBound.width)
-        .attr('height', containerBound.height)
-        .append('g')
-        .attr('id', 'graph-g')
-        .attr(
-          'transform',
-          `translate(${this.margin.left}, ${this.margin.top})`
-        );
-    }
+    const { width, height } = containerNode.getBoundingClientRect();
+    d3.select('#bubble-chart-container')
+      .append('svg')
+      .attr('id', 'bubble-chart-svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('id', 'graph-g')
+      .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
   }
 
-  setYScale() {
-    const node = d3.select('#bubble-chart-container').node() as HTMLElement;
-    if (node) {
-      const containerBound = node.getBoundingClientRect();
-      this.yScale
-        .domain(
-          Object.keys(this.countByCountry).sort((a, b) => {
-            if (a === 'Other') return 1; // Push 'Other' to the end
-            if (b === 'Other') return -1; // Push 'Other' to the end
-            return this.totalByCountry[b] - this.totalByCountry[a]; // Sort by count descending
-          })
-        )
-        .range([
-          0,
-          containerBound.height - this.margin.top - this.margin.bottom,
-        ]);
-    }
+  private setYScale() {
+    const containerNode = d3
+      .select('#bubble-chart-container')
+      .node() as HTMLElement;
+    if (!containerNode) return;
+
+    const { height } = containerNode.getBoundingClientRect();
+    this.yScale
+      .domain(this.getSortedCountries())
+      .range([0, height - this.margin.top - this.margin.bottom]);
   }
 
-  drawYScale() {
+  private drawYScale() {
     d3.select('svg #graph-g')
       .append('g')
       .attr('class', 'y axis')
@@ -166,135 +97,84 @@ export class BubbleChartComponent {
       .call(d3.axisLeft(this.yScale));
   }
 
-  setXScale() {
-    const node = d3.select('#bubble-chart-container').node() as HTMLElement;
-    if (node) {
-      const containerBound = node.getBoundingClientRect();
-      this.xScale
-        .domain(this.yearGroups.map((value) => value.toString()))
-        .range([
-          0,
-          containerBound.width - this.margin.left - this.margin.right,
-        ]);
-    }
+  private setXScales() {
+    const containerNode = d3
+      .select('#bubble-chart-container')
+      .node() as HTMLElement;
+    if (!containerNode) return;
+
+    const { width } = containerNode.getBoundingClientRect();
+    this.xScale
+      .domain(this.yearGroups)
+      .range([0, width - this.margin.left - this.margin.right]);
+
+    this.xScaleCollapse.domain(['Total']).range([0, 0]);
   }
 
-  drawXScale() {
+  private drawXScale() {
+    d3.select('.x').remove();
     d3.select('svg #graph-g')
       .append('g')
       .attr('class', 'x axis')
       .attr('transform', `translate(0, ${-this.margin.top / 2 + 5})`)
-      .call(d3.axisTop(this.xScale));
+      .call(d3.axisTop(this.isTotalShow ? this.xScaleCollapse : this.xScale));
   }
 
-  setXScaleCollapse() {
-    const node = d3.select('#bubble-chart-container').node() as HTMLElement;
-    if (node) {
-      const containerBound = node.getBoundingClientRect();
-
-      this.xScaleCollapse.domain(['Total']).range([0, 0]);
-    }
+  private updateChart() {
+    this.removeScaleLine();
+    this.setMaxDomainValue();
+    this.drawData();
+    this.addTooltip();
   }
 
-  drawXScaleCollapse() {
-    d3.select('svg #graph-g')
-      .append('g')
-      .attr('class', 'x axis')
-      .attr('transform', `translate(0, ${-this.margin.top / 2 + 5})`)
-      .call(d3.axisTop(this.xScaleCollapse));
-  }
-
-  getYearGroup(numGroups: number) {
-    if (this.allYears.size === 0 || numGroups <= 0) return;
-
-    const maxYear = Math.max(...this.allYears);
-    const minYear = Math.min(...this.allYears);
-    const groupSize = Math.ceil(this.allYears.size / numGroups);
-
-    for (let i = 0; i < numGroups; i++) {
-      const groupStart = minYear + i * groupSize;
-      const groupEnd = Math.min(maxYear, groupStart + groupSize - 1);
-      this.yearGroups.push(`${groupStart}-${groupEnd}`);
-
-      if (groupEnd === maxYear) {
-        break;
-      }
-    }
-  }
-
-  getAllYear(data: d3.DSVRowArray<string>) {
-    for (const row of data) {
-      if (!this.allYears.has(Number(row['year']))) {
-        this.allYears.add(Number(row['year']));
-      }
-    }
-  }
-
-  setMaxDomainValue() {
-    if (this.isTotalShow) {
-      this.maxDomainValue = Math.max(
-        ...Object.values(this.totalByCountry).map((total) => total)
-      );
-    } else {
-      this.maxDomainValue = 0;
-      Object.values(this.countByCountry).forEach((country) => {
-        Object.values(country).forEach((count) => {
-          if (this.maxDomainValue < count) {
-            this.maxDomainValue = count;
-          }
-        });
-      });
-    }
-    this.setSizeScale();
-  }
-
-  setSizeScale() {
-    this.sizeScale.domain([0, this.maxDomainValue]).range([3, 40]);
-  }
-
-  drawData() {
+  private drawData() {
     d3.selectAll('.country').remove();
-    if (this.isTotalShow) {
-      d3.select('svg #graph-g')
-        .append('g')
-        .attr('class', 'country')
-        .selectAll('.count')
-        .data(Object.entries(this.totalByCountry))
-        .join('g')
-        .attr('class', 'count')
-        .append('circle')
-        .attr(
-          'transform',
-          (d) =>
-            `translate( ${this.xScaleCollapse('Total')}, ${
-              (this.yScale(d[0]) ?? 0) + this.margin.top / 2
-            })`
-        )
-        .attr('r', (d) => this.sizeScale(d[1]))
-        .style('fill', '#50a1df');
-    } else {
-      d3.select('svg #graph-g')
-        .selectAll('.country')
-        .data(Object.entries(this.countByCountry))
-        .join('g')
-        .attr('class', 'country')
-        .attr(
-          'transform',
-          (d) =>
-            `translate(0, ${(this.yScale(d[0]) ?? 0) + this.margin.top / 2})`
-        )
-        .selectAll('.count')
-        .data((d) => Object.entries(d[1]))
-        .join('g')
-        .attr('class', 'count')
-        .append('circle')
-        .attr('transform', (d) => `translate(${this.xScale(d[0])}, 0)`)
-        .attr('r', (d) => this.sizeScale(d[1]))
-        .style('fill', '#50a1df');
-    }
+
+    this.isTotalShow
+      ? d3
+          .select('svg #graph-g')
+          .selectAll('.country')
+          .data(Object.entries(this.totalByCountry))
+          .join('g')
+          .attr('class', 'country')
+          .attr(
+            'transform',
+            (d) =>
+              `translate(0, ${(this.yScale(d[0]) ?? 0) + this.margin.top / 2})`
+          )
+          .selectAll('.count')
+          .data((d) => ([d]))
+          .join('g')
+          .attr('class', 'count')
+          .append('circle')
+          .attr(
+            'transform',
+            (d) => `translate(${this.xScaleCollapse('Total')}, 0)`
+          )
+          .attr('r', (d) => this.sizeScale(d[1]))
+          .style('fill', '#50a1df')
+      : d3
+          .select('svg #graph-g')
+          .selectAll('.country')
+          .data(Object.entries(this.countByCountry))
+          .join('g')
+          .attr('class', 'country')
+          .attr(
+            'transform',
+            (d) =>
+              `translate(0, ${(this.yScale(d[0]) ?? 0) + this.margin.top / 2})`
+          )
+          .selectAll('.count')
+          .data((d) => (Object.entries(d[1])))
+          .join('g')
+          .attr('class', 'count')
+          .append('circle')
+          .attr('transform', (d) => `translate(${this.xScale(d[0])}, 0)`)
+          .attr('r', (d) => this.sizeScale(d[1]))
+          .style('fill', '#50a1df');
   }
 
-  addTooltip() {
+  private addTooltip() {
     d3.select('.tooltip').remove();
     const tooltip = d3
       .select('#bubble-chart-container')
@@ -310,10 +190,9 @@ export class BubbleChartComponent {
 
     d3.selectAll('circle')
       .on('mouseover', function (event, d) {
-        const data = d as [string, number];
         tooltip
           .style('opacity', 1)
-          .html(`Drafted Players: ${data[1]}`)
+          .html(`Drafted Players: ${(d as number[])[1]}`)
           .style('left', `${event.pageX + 10}px`)
           .style('top', `${event.pageY - 20}px`);
         d3.select(this).style('stroke', '#000').style('stroke-width', 2);
@@ -329,7 +208,7 @@ export class BubbleChartComponent {
       });
   }
 
-  removeScaleLine() {
+  private removeScaleLine() {
     d3.selectAll('.axis path').style('opacity', '0');
     d3.selectAll('.axis text')
       .style('font-family', 'Roboto')
@@ -339,15 +218,96 @@ export class BubbleChartComponent {
   }
 
   onSlideToggle(event: MatSlideToggleChange) {
-    d3.select('.x').remove();
-    if (event.checked) {
-      this.drawXScaleCollapse();
-    } else {
-      this.drawXScale();
+    this.isTotalShow = event.checked;
+    this.drawXScale();
+    this.updateChart();
+  }
+
+  private extractYears(data: d3.DSVRowArray<string>) {
+    data.forEach((row) => this.allYears.add(Number(row['year'])));
+  }
+
+  private generateYearGroups(numGroups: number) {
+    const years = Array.from(this.allYears).sort((a, b) => a - b);
+    const groupSize = Math.ceil(years.length / numGroups);
+
+    for (let i = 0; i < numGroups; i++) {
+      const start = years[i * groupSize];
+      const end = years[Math.min((i + 1) * groupSize - 1, years.length - 1)];
+      this.yearGroups.push(`${start}-${end}`);
     }
-    this.removeScaleLine();
-    this.setMaxDomainValue();
-    this.drawData();
-    this.addTooltip();
+  }
+
+  private aggregateDataByCountry(data: d3.DSVRowArray<string>) {
+    return data.reduce((acc, row) => {
+      const yearGroup = this.yearGroups.find((group) => {
+        const [start, end] = group.split('-').map(Number);
+        return Number(row['year']) >= start && Number(row['year']) <= end;
+      });
+
+      if (!yearGroup) return acc;
+
+      const nationality = row['nationality'];
+      acc[nationality] = acc[nationality] || {};
+      acc[nationality][yearGroup] = (acc[nationality][yearGroup] || 0) + 1;
+
+      return acc;
+    }, {} as Record<string, Record<string, number>>);
+  }
+
+  private calculateTotalByCountry(
+    countByCountry: Record<string, Record<string, number>>
+  ) {
+    return Object.entries(countByCountry).reduce((acc, [country, years]) => {
+      acc[country] = Object.values(years).reduce(
+        (sum, count) => sum + count,
+        0
+      );
+      return acc;
+    }, {} as Record<string, number>);
+  }
+
+  private filterTopCountries(
+    countByCountry: Record<string, Record<string, number>>
+  ) {
+    const sortedCountries = Object.entries(this.totalByCountry).sort(
+      (a, b) => b[1] - a[1]
+    );
+    const top7 = sortedCountries.slice(0, 7);
+    const others = sortedCountries.slice(7);
+
+    const othersCount = others.reduce((acc, [country]) => {
+      Object.entries(countByCountry[country]).forEach(([year, count]) => {
+        acc[year] = (acc[year] || 0) + count;
+      });
+      return acc;
+    }, {} as Record<string, number>);
+
+    this.totalByCountry = Object.fromEntries([
+      ...top7,
+      ['Other', others.reduce((sum, [, count]) => sum + count, 0)],
+    ]);
+    return Object.fromEntries([
+      ...top7.map(([country]) => [country, countByCountry[country]]),
+      ['Other', othersCount],
+    ]);
+  }
+
+  private getSortedCountries() {
+    return Object.keys(this.countByCountry).sort((a, b) => {
+      if (a === 'Other') return 1;
+      if (b === 'Other') return -1;
+      return this.totalByCountry[b] - this.totalByCountry[a];
+    });
+  }
+
+  private setMaxDomainValue() {
+    const values = this.isTotalShow
+      ? Object.values(this.totalByCountry)
+      : Object.values(this.countByCountry).flatMap((country) =>
+          Object.values(country)
+        );
+    this.maxDomainValue = Math.max(...values);
+    this.sizeScale.domain([0, this.maxDomainValue]).range([3, 40]);
   }
 }
